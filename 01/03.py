81b0cf5158d1a34bf4b752b51fd8a11df9c1f2ec
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-02 01:14:20 +0530

    print

--- 01/03.py
+++ 01/03.py
@@ -7,3 +7,7 @@ print(90)
 firstName = input("enter your first name \n")
 lastName = input("enter your last name \n")
 print("Hello" + " " + firstName + " " + lastName )
+
+# length of a string
+
+print(len(lastName))
