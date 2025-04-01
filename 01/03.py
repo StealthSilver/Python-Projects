c6c02c0baa926b43f037c8c9213e9a8923ea834d
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-02 01:12:12 +0530

    print

---
+++ 01/03.py
@@ -0,0 +1,9 @@
+# variables
+
+num = 90
+print(90)
+
+# printing using variables
+firstName = input("enter your first name \n")
+lastName = input("enter your last name \n")
+print("Hello" + " " + firstName + " " + lastName )
