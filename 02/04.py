ea6f2e4067bb6798451f76bf6893e5725fa921b2
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-03 11:12:57 +0530

    print

---
+++ 02/04.py
@@ -0,0 +1,22 @@
+# mathematical operations
+
+# Addition
+print(3 + 5)
+
+# Substraction
+print(5 - 3)
+
+# Multiplication
+print(8 * 9)
+
+# division will always give you floating point number due to implicit type casting 
+print(9 / 3)
+
+# to avoid implicit type casting use (//)
+print(9//3)
+
+# Exponentiation
+print(5**4)
+
+# Modulus
+print(8%3)
