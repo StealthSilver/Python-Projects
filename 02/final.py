4c7fe9e78733276d8e0ba8121f9f8acfffe8609b
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-03 23:22:56 +0530

    print

--- 02/final.py
+++ 02/final.py
@@ -2,10 +2,10 @@
 
 print("Welcome to the tip calculator")
 
-bill = int(input("what was the total bill? $"))
+bill = float(input("what was the total bill? $"))
 tip = int(input("Huw mush tip woult you like to give? 10, 12, or 15? "))
 people = int(input("How many people to split the bill? "))
 
-total = str((((bill / 100) * tip) / people))
+total = str((((bill / 100) * tip) / people) + bill)
 
 print("Each person should pay: $" + total)
