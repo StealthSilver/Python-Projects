8c66560b13440a0e34368b480147e78aed92e950
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-03 11:04:52 +0530

    print

--- 02/01.py
+++ 02/01.py
@@ -6,3 +6,7 @@ print("Hello"[-1])
 
 print("123" + "345")
 print(123 + 345)
+
+# to format large numbers use _
+
+print(123_345 + 123_678)
---
+++ 02/02.py
@@ -0,0 +1,16 @@
+# string 
+
+print("Hello")
+
+# int 
+
+print(65)
+
+# float 
+
+print(43.5)
+
+# boolean
+
+print(True)
+print(False)
