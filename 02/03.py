5d43b5612a7ebb455b8cc2222fc8be2c8b752237
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-03 11:09:27 +0530

    print

--- 02/03.py
+++ 02/03.py
@@ -4,3 +4,10 @@ print(type(324))
 print(type("hello"))
 print(type(43.23))
 print(type(True))
+
+# type conversion 
+
+print(type(int("321")))
+print(type(bool("True")))
+
+# we cannot type convert bigger data types to saller data types
