7acc9d3659195dd7218e007721316494fb93747d
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-04 10:44:40 +0530

    print

--- 03/03.py
+++ 03/03.py
@@ -4,8 +4,10 @@ height = int(input("enter your height"))
 age = int(input("enter your age"))
 
 if height >= 130:
-    if age >= 7:
+    if age <= 12:
         print("You need to pay $12")
+    elif age <= 18:
+        print("you need to pay $10") 
     else:
         print("you need to pay $7")
 else:
