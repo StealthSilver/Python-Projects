72b911addb05fab5aa1ee4cacf941293c8e42cb8
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-04 10:37:17 +0530

    print

---
+++ 03/01.py
@@ -0,0 +1,9 @@
+# Conditional Statement
+
+print("Welcome to the rollercoster")
+height = int(input("Enter you height in cm : "))
+
+if height > 130:
+    print("you can ride the rollercoster")
+else:
+    print("Sorry, you cannot ride the rollercoster")
