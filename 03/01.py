0d35af8bed211a15cbd2e5d9460f42d281700daa
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-04 10:37:44 +0530

    print

--- 03/01.py
+++ 03/01.py
@@ -3,7 +3,7 @@
 print("Welcome to the rollercoster")
 height = int(input("Enter you height in cm : "))
 
-if height > 130:
+if height >= 130:
     print("you can ride the rollercoster")
 else:
     print("Sorry, you cannot ride the rollercoster")
