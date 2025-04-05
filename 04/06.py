b9c4701d76c0e9e68df9f107646de9c70762ac06
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-05 10:11:32 +0530

    04

--- 04/06.py
+++ 04/06.py
@@ -6,5 +6,7 @@ friends = ["Pogo" , "Tom" , "Mony" , "Jaadu"]
 
 num = random.randint(0,3)
 
+# we can also use random.choice(friends) to get the random name
+
 pay = friends[num]
 print(f"Today {pay} will pay the bill")
