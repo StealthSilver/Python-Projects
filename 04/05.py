6c2f0dbb93310be4fe28041d0104443214b70fa9
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-05 10:06:34 +0530

    print

--- 04/05.py
+++ 04/05.py
@@ -7,6 +7,17 @@ print(fruits[0])
 print(fruits[2])
 print(fruits[-1])
 
+
+# changing the element in the list
 fruits[2] = "Grapes"
+
+print(fruits)
+
+# adding elements to the end of list 
+fruits.append("Pineapple")
+print(fruits)
+
+# adding a list to the end of list 
+fruits.extend(["Papaya" , "Mango"])
 
 print(fruits)
