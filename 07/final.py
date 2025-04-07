b2e0727a90366aa7ae6f84c46cfb72a1ec95d8f4
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-07 11:48:27 +0530

    04

--- 07/final.py
+++ 07/final.py
@@ -2,17 +2,6 @@ import random
 
 fruits = ['APPLE' , 'BANANA' , 'ORANGE' , 'MANGO' , 'GRAPES']
 
-name = fruits[random.randint(1,4)]
+name = random.choice(fruits)
+print(name)
 
-guess = []
-n = 0
-while(n <= 4):
-    inp = input("ENTER THE FIRST LETTER : ")
-    guess.append(inp)
-    n += 1
-
-
-if name == str(guess):
-    print("won")
-else:
-    print("lost")
