56bc698924c5b7d4323ed1a5550b152205386a9b
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-07 11:53:33 +0530

    04

--- 07/final.py
+++ 07/final.py
@@ -2,8 +2,14 @@ import random
 
 fruits = ['APPLE' , 'BANANA' , 'ORANGE' , 'MANGO' , 'GRAPES']
 
-name = random.choice(fruits)
-print(name)
+chosen_word = random.choice(fruits)
+print(chosen_word)
 
-guess = input("Guess a letter : ").lower()
+guess = input("Guess a letter : ").upper()
 
+
+for letter in chosen_word:
+    if letter == guess:
+        print("right")
+    else: 
+        print("wrong")
