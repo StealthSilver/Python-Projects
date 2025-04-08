e20216bbb22756856aa11fe55c6bcf169652de5d
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-08 16:56:38 +0530

    ollie

---
+++ 09/02.py
@@ -0,0 +1,13 @@
+# Looping the dictionary 
+
+user = {
+    "Name" : "Pogo",
+    "Age" : 34,
+    "City" : "London",
+    "Driving" : True
+}
+
+# prints the key
+for thing in user:
+    print(thing)
+    print(user[thing])
