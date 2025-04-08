8590c6f16b58e3741797c3ab795938f0842fcd7f
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-08 17:22:12 +0530

    nesting

--- 09/final.py
+++ 09/final.py
@@ -1,10 +1,6 @@
 # First Price Bid
 
-name = input("Enter your name : ")
-price = int(input("Enter the bid price : $ "))
-
 bids = {}
-bids[name] = price
 
 continue_bidding = True
 
@@ -12,8 +8,21 @@ while continue_bidding:
     name = input("Enter your name : ")
     price = int(input("Enter the bid price : $ "))
     bids[name] = price 
-    should_continue = input("Are there any bidders ? Type 'yes' or 'no' : ")
+    should_continue = input("Are there any bidders ? Type 'yes' or 'no' : ").lower
+    if should_continue == "no":
 
+
+
+def find_highest_bidder(bidding_dictionary):
+
+    highest_bid = 0
+    for bidder in bidding_dictionary:
+        bid_amount = bidding_dictionary[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    
+    print(f"the winner is {winner} with a bid of ${highest_bid}.")
 
 # for clearing the screen
 print("\n"*100)
