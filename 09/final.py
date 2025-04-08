e52aa524b83bed1dbaf4929507b5ef2b45ffd2e5
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-08 17:16:53 +0530

    nesting

--- 09/final.py
+++ 09/final.py
@@ -1,5 +1,18 @@
 # First Price Bid
 
+bids = {}
+
+
+name = input("Enter your name : ")
+price = int(input("Enter the bid price : $ "))
+
+
+
+bids[name] = price
+
+
+should_continue = input("Are there any bidders ? Type 'yes' or 'no' : ")
+
 
 # for clearing the screen
 print("\n"*100)
