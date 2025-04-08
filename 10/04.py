33c99c2e1df43d20cc7b9862933f181610230b71
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-09 01:21:53 +0530

    nesting

--- 10/04.py
+++ 10/04.py
@@ -1,1 +1,9 @@
-# the return keyword is the ending of the function
+def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        # early return 
+        return
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    return f"Result: {formated_f_name} {formated_l_name}"
+
+print(format_name(input("What is the first name: "), input("What is the last name: ")))
