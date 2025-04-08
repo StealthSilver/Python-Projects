9d63d7a1bc3fa8b4fa125cddc043f8fefd6d8423
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-08 17:08:37 +0530

    nesting

--- 09/04.py
+++ 09/04.py
@@ -15,3 +15,15 @@ print(travel_log["France"][1])
 nested_list = ["A" , "B" , ["C" , "D"]]
 
 print(nested_list[2][1])
+
+nested_dict = {
+    "France" : {
+        "times_visited" : 2,
+        "cities" : ["Paris" , "lille" , "Dijon"],
+    },
+      "Germany" : {
+        "times_visited" : 1,
+        "cities" : ["Stuttgart", "Berlin"],
+    },
+
+}
