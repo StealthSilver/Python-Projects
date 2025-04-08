e88b92f28a4f480f63ec79deb89dd29fce20e6c9
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-08 17:27:02 +0530

    nesting

--- 09/final.py
+++ 09/final.py
@@ -1,5 +1,3 @@
-# First Price Bid
-
 bids = {}
 
 def find_highest_bidder(bidding_dictionary):
@@ -11,20 +9,18 @@ def find_highest_bidder(bidding_dictionary):
             highest_bid = bid_amount
             winner = bidder
     
-    print(f"the winner is {winner} with a bid of ${highest_bid}.")
+    print(f"The winner is {winner} with a bid of ${highest_bid}.")
 
 continue_bidding = True
 
 while continue_bidding:
-    name = input("Enter your name : ")
-    price = int(input("Enter the bid price : $ "))
+    name = input("Enter your name: ")
+    price = int(input("Enter the bid price: $ "))
     bids[name] = price 
-    should_continue = input("Are there any bidders ? Type 'yes' or 'no' : ").lower
+    should_continue = input("Are there any other bidders? Type 'yes' or 'no': ").lower() 
     if should_continue == "no":
         continue_bidding = False
         find_highest_bidder(bids)
     elif should_continue == "yes":
-        # for clearing the screen
-        print("\n"*100)
-
-
+        # Simulate screen clearing 
+        print("\n" * 100)
