0643d31d0b444739046daacde4592536a61a8d05
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-09 10:29:15 +0530

    nesting

--- 11/final.py
+++ 11/final.py
@@ -5,14 +5,16 @@ cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
 user = input("Do you want to play a game of Blackjack? type 'y' or 'n': " )
 
 initial_cards = random.sample(cards, 2) 
-
+comp_card = random.sample(cards,1)
 
 score = sum(initial_cards)
+comp_score = sum(comp_card)
 
 
 def blackjack_game():
     if user == 'y':
         print(f"your cards: {initial_cards}, current score: {score}")
+        print(f"Computer's first card : {comp_score}")
     else:
         return 
     
