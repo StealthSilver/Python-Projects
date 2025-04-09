3f653737b4d6739d4c971b13ef05ed625de63991
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-09 10:28:16 +0530

    nesting

--- 11/final.py
+++ 11/final.py
@@ -5,7 +5,7 @@ cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
 user = input("Do you want to play a game of Blackjack? type 'y' or 'n': " )
 
 initial_cards = random.sample(cards, 2) 
-print(initial_cards)
+
 
 score = sum(initial_cards)
 
