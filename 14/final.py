12a8d77289dc0437f47dfabd92383e417e200678
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-10 22:45:54 +0530

    nesting

--- 14/final.py
+++ 14/final.py
@@ -1,39 +1,41 @@
-# Higher Lower Game
-
-import data 
+import data
 import random
 
-print("Welcome to higher lower game \n")
-
-a = random.randint(0,20)
-b = random.randint(0,20)
-
-first = data.game_data[a]["name"]
-second = data.game_data[b]["name"]
-
-first_d = data.game_data[a]["description"]
-second_d = data.game_data[b]["description"]
-
-first_f = data.game_data[a]["followers"]
-second_f = data.game_data[b]["followers"]
-
-print(f"Compare A : {first}, {first_d} \n")
-print(f"Against B : {second}, {second_d} \n")
-
-user = input("Who has more followers? Type 'A' or 'B' : ").upper()
+print("Welcome to the Higher Lower Game!\n")
 
 score = 0
-game = True 
+game = True
 
+
+def get_random_account(exclude=None):
+    while True:
+        account = random.choice(data.game_data)
+        if account != exclude:
+            return account
+
+account_a = get_random_account()
+account_b = get_random_account(exclude=account_a)
 
 while game:
-    if user == "A" and first_f > second_f:
-        print(f"you won, {first} has {first_f} followers and {second} has {second_f} followers")
+    print(f"Compare A: {account_a['name']}, {account_a['description']}")
+    print("vs")
+    print(f"Compare B: {account_b['name']}, {account_b['description']}\n")
+    
+    user = input("Who has more followers? Type 'A' or 'B': ").upper()
+
+    a_followers = account_a['followers']
+    b_followers = account_b['followers']
+
+    if (user == "A" and a_followers > b_followers) or (user == "B" and b_followers > a_followers):
         score += 1
-    elif user == "B" and first_f < second_f:
-        print(f"you won, {first} has {first_f} followers and {second} has {second_f} followers")
-        score += 1
+        print(f"You won! Current score: {score}\n")
+
+        
+        if user == "A":
+            account_b = get_random_account(exclude=account_a)
+        else:
+            account_a = account_b
+            account_b = get_random_account(exclude=account_a)
     else:
-        print("You Lost")
-        print(score)
+        print(f"You lost! Final score: {score}")
         game = False
