5d23c3982b9a7bfee39008ca79447ce923e0616c
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-10 11:22:42 +0530

    nesting

--- 12/final.py
+++ 12/final.py
@@ -1,22 +1,34 @@
-# Number Guessing game
-
 import random
 
-print("Welcome to the numebr gussing game")
+print("Welcome to the number guessing game!")
 print("Computer chooses a number between 1 and 100")
+
+guess = random.randint(1, 100)
+
+
+diff = input("Choose your difficulty 'e' (easy) or 'h' (hard): ").lower()
+
+if diff == 'e':
+    chances = 10
+else:
+    chances = 5
 
 game = True
 
-guess = random.randint(0,100)
-print(guess)
-
-
 while game:
-    user = int(input("guess a number between 1 and 100"))
-    if user > guess:
-        print("too high")
-    elif user < guess:
-        print("too low")
-    else:
-        print("you guessed the numebr you won")
+    for n in range(chances):
+        print(f"\nYou have {chances - n} attempts remaining.")
+        user = int(input("Guess a number between 1 and 100: "))
+
+        if user > guess:
+            print("Too high.")
+        elif user < guess:
+            print("Too low.")
+        else:
+            print(f"You guessed the number {guess}! You won 🎉")
+            game = False
+            break
+
+    if game:  
+        print(f"\nYou've run out of guesses. You lose 😢 The number was {guess}")
         game = False
