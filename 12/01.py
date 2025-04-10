f94cde239f19f4d1847dda8cc919b4cf6cac0d28
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-10 10:18:51 +0530

    nesting

--- 12/01.py
+++ 12/01.py
@@ -1,2 +1,12 @@
 # Scope
 
+enemies = 1
+
+def increase_enemies():
+    # local scope
+    enemies = 2
+    print(f"enemies inside the function {enemies}")
+
+
+increase_enemies()
+print(f"enemies outside the function {enemies}")
