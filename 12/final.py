5715ad77b92bc9d9604e8054c899f76250377edd
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-10 11:14:47 +0530

    nesting

--- 12/final.py
+++ 12/final.py
@@ -1,4 +1,11 @@
 # Number Guessing game
 
+import random
+
 print("Welcome to the numebr gussing game")
 print("Computer chooses a number between 1 and 100")
+
+game = True
+
+guess = random.randint(100)
+print(guess)
