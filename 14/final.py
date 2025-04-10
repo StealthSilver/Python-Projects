ecdabd759debf83e1546d534100bbacabe843bcd
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-10 11:51:25 +0530

    nesting

--- 14/final.py
+++ 14/final.py
@@ -5,4 +5,11 @@ import random
 
 print("Welcome to higher lower game")
 
-user = input("Who has more followers? Type 'A' or 'B' : ")
+user = input("Who has more followers? Type 'A' or 'B' : ").upper()
+
+score = 0
+game = True 
+
+first = data[random.randint(0,10)]["name"]
+print("first")
+
