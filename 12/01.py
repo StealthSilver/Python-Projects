9805d4eaf945a3a85f5d4076743875e167de249f
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-10 10:19:28 +0530

    nesting

--- 12/01.py
+++ 12/01.py
@@ -4,7 +4,7 @@
 enemies = 1
 
 def increase_enemies():
-    # local scope
+    # local scope -> valid only inside the function
     enemies = 2
     print(f"enemies inside the function {enemies}")
 
