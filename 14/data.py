f5d3602f16ce66698a46118ce0c73ba59d6b52b4
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-10 11:48:27 +0530

    nesting

---
+++ 14/data.py
@@ -0,0 +1,102 @@
+data = [
+    {
+        "name": "Cristiano Ronaldo",
+        "followers": "634,000,000",
+        "description": "Portuguese Footballer"
+    },
+    {
+        "name": "Lionel Messi",
+        "followers": "515,000,000",
+        "description": "Argentinian Footballer"
+    },
+    {
+        "name": "Selena Gomez",
+        "followers": "430,000,000",
+        "description": "American Singer & Actress"
+    },
+    {
+        "name": "Kylie Jenner",
+        "followers": "400,000,000",
+        "description": "American Media Personality"
+    },
+    {
+        "name": "Dwayne Johnson",
+        "followers": "395,000,000",
+        "description": "American Actor & Wrestler"
+    },
+    {
+        "name": "Ariana Grande",
+        "followers": "380,000,000",
+        "description": "American Singer & Actress"
+    },
+    {
+        "name": "Kim Kardashian",
+        "followers": "370,000,000",
+        "description": "American Businesswoman"
+    },
+    {
+        "name": "Beyoncé",
+        "followers": "320,000,000",
+        "description": "American Singer"
+    },
+    {
+        "name": "Khloé Kardashian",
+        "followers": "310,000,000",
+        "description": "American TV Personality"
+    },
+    {
+        "name": "Nike",
+        "followers": "305,000,000",
+        "description": "Sportswear Brand"
+    },
+    {
+        "name": "Kendall Jenner",
+        "followers": "290,000,000",
+        "description": "American Model"
+    },
+    {
+        "name": "Justin Bieber",
+        "followers": "290,000,000",
+        "description": "Canadian Singer"
+    },
+    {
+        "name": "National Geographic",
+        "followers": "285,000,000",
+        "description": "Nature Photography"
+    },
+    {
+        "name": "Taylor Swift",
+        "followers": "270,000,000",
+        "description": "American Singer-Songwriter"
+    },
+    {
+        "name": "Jennifer Lopez",
+        "followers": "260,000,000",
+        "description": "American Singer & Actress"
+    },
+    {
+        "name": "Virat Kohli",
+        "followers": "255,000,000",
+        "description": "Indian Cricketer"
+    },
+    {
+        "name": "Nicki Minaj",
+        "followers": "240,000,000",
+        "description": "Trinidadian Rapper"
+    },
+    {
+        "name": "Miley Cyrus",
+        "followers": "230,000,000",
+        "description": "American Singer"
+    },
+    {
+        "name": "Kourtney Kardashian",
+        "followers": "220,000,000",
+        "description": "American Socialite"
+    },
+    {
+        "name": "Zendaya",
+        "followers": "215,000,000",
+        "description": "American Actress"
+    }
+]
---
+++ 14/final.py
@@ -0,0 +1,7 @@
+# Higher Lower Game
+
+import data 
+import random
+
+print("Welcome to higher lower game")
+
