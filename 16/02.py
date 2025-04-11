e80b0a8c9d80d0327f2faca1f4f50f419eb05990
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-11 12:28:25 +0530

    pip

--- 16/02.py
+++ 16/02.py
@@ -4,8 +4,12 @@ from prettytable import PrettyTable  # type: ignore
 
 table = PrettyTable()
 
+# Methods
 table.add_column("Pokemon Name" , ["Pikachu" , "Charmander" , "Squirtle"])
 table.add_column("Type" , ["Electric" , "Water" , "Fire"])
+
+# Attributes
+table.align = 'l'
 
 print(table)
 
