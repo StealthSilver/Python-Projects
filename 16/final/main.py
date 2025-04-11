c6318e7f64b1af9a4d8c790904938f5655704ce4
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-11 12:40:20 +0530

    pip

--- 16/final/main.py
+++ 16/final/main.py
@@ -4,7 +4,13 @@ from money_machine import MoneyMachine
 
 money_machine = MoneyMachine()
 coffee_maker = CoffeeMaker()
+menu = Menu()
 
+is_on = True
 
 money_machine.report()
 coffee_maker.report()
+
+
+while is_on:
+
