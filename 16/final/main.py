41cb977c1e0d0f05ec92020763a62ee570347da9
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-11 12:44:45 +0530

    pip

--- 16/final/main.py
+++ 16/final/main.py
@@ -23,4 +23,5 @@ while is_on:
     else:
         drink = menu.find_drink(choice)
         if coffee_maker.is_resource_sufficient(drink):
-            print(money_machine.make_payment(drink.cost))
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
