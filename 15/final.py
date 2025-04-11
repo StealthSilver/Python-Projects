c162927b37b1bd06741650ba1f5563069346d5d3
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-11 11:25:18 +0530

    nesting

--- 15/final.py
+++ 15/final.py
@@ -1,2 +1,27 @@
 # Coffee Machine 
 
+Coffee = [
+    {
+        "name" : "Espresso",
+        "price": "$1.50",
+        "water" : "50ml",
+        "coffee" : "18g",
+        "milk":"0ml"
+    }, 
+    {
+        "name" : "Latte",
+        "price":"$2.50",
+        "water":"200ml",
+        "coffee":"24g",
+        "milk":"150ml"
+    },
+    {
+        "name" : "Cappuccino",
+        "price":"$3.00",
+        "water":"250ml",
+        "coffee":"24g",
+        "milk":"100ml"
+
+    }
+]
+    
