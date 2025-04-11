3bdfa98695a9ade6be56beea519352290d60c61b
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-11 12:46:57 +0530

    pip

--- 16/final/main.py
+++ 16/final/main.py
@@ -4,6 +4,7 @@ from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+# constructing the objects
 money_machine = MoneyMachine()
 coffee_maker = CoffeeMaker()
 menu = Menu()
