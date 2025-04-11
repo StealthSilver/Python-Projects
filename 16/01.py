6638eea543a16e5294d954ad10dbc8f4ed76f59f
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-11 12:15:52 +0530

    nesting

--- 16/01.py
+++ 16/01.py
@@ -4,5 +4,8 @@ from turtle import Turtle , Screen
 timmy = Turtle()
 print(timmy)
 
+# screen is an object
 my_screen  = Screen()
+
+# the canvas height is the attribute
 print(my_screen.canvheight)
