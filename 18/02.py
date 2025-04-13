1fc062bcdfbe181a0bf261e04ba206fde253de7b
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-13 13:02:41 +0530

    pip

---
+++ 18/02.py
@@ -0,0 +1,17 @@
+# types of imports 
+
+
+# Basic import
+import turtle 
+tim = turtle.Turtle()
+
+
+# for multiple imports
+from turtle import Turtle 
+
+tom = Turtle()
+pop = Turtle()
+
+# importing everything 
+
+from turtle import *
