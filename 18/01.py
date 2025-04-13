e2f68b33a9b5078421e0dae790ce57262d66989e
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-13 12:58:48 +0530

    pip

--- 18/01.py
+++ 18/01.py
@@ -6,12 +6,14 @@ timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("red")
 
-
-
-
-
-
-
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
 
 screen = Screen()
 screen.exitonclick()
