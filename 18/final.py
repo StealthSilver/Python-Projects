ea100b90bbeb3fd7b03811be9489f6b474a0edfe
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-13 13:53:53 +0530

    pip

--- 18/final.py
+++ 18/final.py
@@ -1,2 +1,15 @@
 # creating the Hirst painting
 
+from turtle import Turtle, Screen
+import random 
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("black")
+timmy.speed("fastest") 
+
+screen = Screen()
+screen.colormode(255)
+
+
+screen.exitonclick()
