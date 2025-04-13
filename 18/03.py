d25ae0fca0135b2379209a6feb72c8d284f8e52f
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-13 13:13:18 +0530

    pip

---
+++ 18/03.py
@@ -0,0 +1,16 @@
+# drawing the dashed line 
+
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("black")
+
+for _ in range(20):
+    timmy.forward(10)
+    timmy.color("white")
+    timmy.forward(10)
+    timmy.color("black")
+
+screen = Screen()
+screen.exitonclick()
