a8a99e803135470ba9df6efd855939bb4113aeb7
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-13 12:09:54 +0530

    pip

--- 17/02.py
+++ 17/02.py
@@ -1,8 +1,14 @@
 class User:
-    pass
+    
+    def __init__(self):
+        print("new user being created")    
 
 user_1 = User()
 user_1.id = "001"
 user_1.username = "pogo"
 
 print(user_1.username)
+
+user_2 = User()
+user_2.id = "002"
+user_2.username = "rans"
