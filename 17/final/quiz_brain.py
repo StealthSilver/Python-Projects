76277c01236110212a4bda3e25061163e4cf4780
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-13 12:46:12 +0530

    pip

--- 17/final/quiz_brain.py
+++ 17/final/quiz_brain.py
@@ -4,6 +4,7 @@ class QuizBrain():
 
     def __init__(self , q_list):
         self.question_number = 0
+        self.score = 0
         self.question_list = q_list
 
     def still_has_questions(self):
@@ -17,7 +18,9 @@ class QuizBrain():
         
     def check_answer(self, user_answer , correct_answer):
         if user_answer.lower() == correct_answer.lower():
+            self.score+=1
             print("you got it right !")
         else:
             print("that's wrong")
         print(f"the correct answer was: {correct_answer}.")
+        print(f"your current score is {self.score} / {self.question_number}")
