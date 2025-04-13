0466ee40657c0630bb94fc788b1215f15182ac18
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-13 12:19:41 +0530

    pip

--- 17/03.py
+++ 17/03.py
@@ -9,6 +9,7 @@ class User:
         self.id = user_id
         self.username = username
         self.followers = 0
+        self.following = 0
 
     def follow(self, user):
         user.followers += 1
