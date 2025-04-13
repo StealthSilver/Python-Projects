f9eb092e4b3ad86ebd6d173c9cdd97bce8655672
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-13 13:32:18 +0530

    pip

---
+++ 18/06.py
@@ -0,0 +1,19 @@
+# random movement of turtle
+
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("black")
+timmy.speed("fastest") 
+
+colors = [
+    "red", "blue", "green", "yellow", "orange", "purple", "pink", "cyan",
+    "magenta", "brown", "black", "gray", "turquoise", "gold", "navy",
+    "lime", "maroon", "coral", "indigo", "violet"
+]
+
+
+
+screen = Screen()
+screen.exitonclick()
