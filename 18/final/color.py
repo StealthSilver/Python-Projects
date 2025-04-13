793b4b10c82a3e3654f686be81343329e45a7bfb
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-13 14:02:35 +0530

    pip

---
+++ 18/final/color.py
@@ -0,0 +1,14 @@
+# getting the colors for the Hirst painting
+
+import colorgram 
+
+rgb_colors = []
+colors = colorgram.extract('demo.jpeg' , 35)
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    new_color = (r,g,b)
+    rgb_colors.append(new_color)
+
+print(rgb_colors)
