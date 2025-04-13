c5d0bd3c07d8d2ab1928ad161c1270f051432b97
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-13 12:14:06 +0530

    pip

--- 17/02.py
+++ 17/02.py
@@ -5,10 +5,12 @@ class User:
         print("new user being created")
         self.id = user_id
         self.username = username
+        # default value for a object
         self.followers = 0
 
 user_1 = User("001" ,"pogo")
 print(user_1.username)
+print(user_1.followers)
 
 user_2 = User("002", "rans")
 print(user_2.id)
