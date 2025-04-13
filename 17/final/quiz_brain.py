c941d5110ff5e4082ffdd5336f070711adb76aad
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-13 12:35:45 +0530

    pip

--- 17/final/quiz_brain.py
+++ 17/final/quiz_brain.py
@@ -1,2 +1,10 @@
 # the quizzing functonality 
 
+class QuizBrain():
+
+    def __init__(self , q_list):
+        self.question_number = 0
+        self.question_list = q_list
+        
+    def next_question():
+        
