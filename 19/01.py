387b951ed80cf1df028b9d62e7e4441cda09df5f
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-14 12:31:12 +0530

    event listners

--- 19/01.py
+++ 19/01.py
@@ -9,6 +9,8 @@ def move_forwards():
     tim.forward(10)
 
 screen.listen()
+
+# higher order function -> function as an input
 screen.onkey(key = "space" , fun = move_forwards)
 
 screen.exitonclick()
