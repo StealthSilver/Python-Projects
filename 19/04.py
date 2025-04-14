17306cc4cf89365d1cad282348cc1e0a6cc82a6c
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-14 12:44:07 +0530

    event listners

--- 19/04.py
+++ 19/04.py
@@ -8,3 +8,7 @@ screen = Screen()
 screen.setup(width = 500 , height = 400)
 
 user_bet = screen.textinput(title = "make your bet" , prompt = "which turtle will win the race: Enter a color : ")
+
+
+
+screen.exitonclick()
