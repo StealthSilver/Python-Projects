794b5bb2a2b394037852420b57edbf5f34364eb4
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-14 12:48:23 +0530

    event listners

--- 19/04.py
+++ 19/04.py
@@ -8,10 +8,12 @@ screen = Screen()
 screen.setup(width = 500 , height = 400)
 
 user_bet = screen.textinput(title = "make your bet" , prompt = "which turtle will win the race: Enter a color : ")
+colors = ['red' , 'green' , 'yellow' , 'blue' , 'orange' , 'purple']
+y_positions = [-70 , -40 , -10 , 20 , 50 , 80]
 
-
-tim = Turtle(shape = "turtle")
-tim.penup()
-tim.goto(x = -230 , y = -100)
+for turtle_index in range(0,6):
+    tim = Turtle(shape = "turtle")
+    tim.penup()
+    tim.goto(x = -230 , y = y_positions[turtle_index])
 
 screen.exitonclick()
