b90bc4bf041e1797143570d18f6c350fc2bd727b
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-14 12:44:18 +0530

    event listners

--- 19/04.py
+++ 19/04.py
@@ -2,13 +2,15 @@
 
 from turtle import Turtle , Screen 
 
-tim = Turtle()
+
 screen = Screen()
 
 screen.setup(width = 500 , height = 400)
 
 user_bet = screen.textinput(title = "make your bet" , prompt = "which turtle will win the race: Enter a color : ")
 
+
+tim = Turtle()
 
 
 screen.exitonclick()
