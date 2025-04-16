76ee11e02ef89d40ed95e87504edc0582e86f4d6
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-16 18:40:45 +0530

    event listners

--- 20/main.py
+++ 20/main.py
@@ -9,6 +9,12 @@ screen.title("Snake Game")
 segment_1 = Turtle("square")
 segment_1.color("white")
 
+segment_2 = Turtle("square")
+segment_2.color("white")
+segment_2.goto(-20, 0)
 
+segment_3 = Turtle("square")
+segment_3.color("white")
+segment_3.goto(-40,0)
 
 screen.exitonclick()
