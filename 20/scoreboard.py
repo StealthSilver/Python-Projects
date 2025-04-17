f44416f7b7dfcfae9088529c6d5b23f039191e87
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-18 00:03:20 +0530

    refactoring

--- 20/scoreboard.py
+++ 20/scoreboard.py
@@ -14,10 +14,13 @@ class Scoreboard(Turtle):
     def update_scoreboard(self):
         self.write(f"Score: {self.score}" , align="center", font = ("Arial" , 24 , "normal"))
 
-        
+    def game_over(self):
+        self.goto(0,0)
+        self.write("GAME OVER" , align = "center" , font = ("Arial" , 24 , "normal"))
 
     def inrease_score(self):
         self.score += 1
         self.clear()
         self.update_scoreboard()
         
+    
