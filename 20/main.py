9ef343cf8fc94c001bed26d521902537d3766e9f
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-17 16:23:46 +0530

    event listners

--- 20/main.py
+++ 20/main.py
@@ -28,10 +28,9 @@ while game_is_on:
    screen.update()
    time.sleep(0.1)
    
-   for seg_num in range(start =  2, stop = 0 , step = -1):
+   for seg_num in range(len(segments)- 1 , 0 , -1):
        new_x = segments[seg_num -1].xcor()
        new_y = segments[seg_num - 1].ycor()
-
        segments[seg_num].goto(new_x , new_y)
 
 screen.exitonclick()
