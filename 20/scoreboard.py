aa34751be844eb54dccd21dac8794615d0307b6f
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-17 17:17:05 +0530

    refactoring

--- 20/scoreboard.py
+++ 20/scoreboard.py
@@ -5,4 +5,5 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.score("white")
         self.write(f"Score: {self.score}" , aligh = "center" , font = ("Arial" , 24 , "normal"))
