357d72ef5dd652bb02c355c9d8c1ff51380fbf66
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-18 00:02:02 +0530

    refactoring

--- 20/main.py
+++ 20/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -28,6 +29,12 @@ while game_is_on:
     # detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        Scoreboard.increase_score()
+
+    # detect collision with wall
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
 
 screen.exitonclick()
 
