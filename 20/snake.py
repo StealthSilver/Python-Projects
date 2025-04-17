62542325132dfe663fc2418622396cceee7b8cb8
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-17 16:33:10 +0530

    refactoring

--- 20/snake.py
+++ 20/snake.py
@@ -20,7 +20,6 @@ class Snake:
                 self.segments.append(new_segment)
 
         def move(self):
-
             for seg_num in range(len(self.segments) - 1, 0, -1):
                 new_x = self.segments[seg_num - 1].xcor()
                 new_y = self.segments[seg_num - 1].ycor()
