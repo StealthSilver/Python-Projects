ef9d4f00ae9469ee9dbcf13d60a952c7cb9bfcd9
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-20 08:02:06 +0530

    refactoring

---
+++ 21/ball.py
@@ -0,0 +1,10 @@
+from turtle import Turtle
+
+class Ball(Turtle):
+
+    def __itit__(self):
+
+        super().__init__()
+        self.color("white")
+        self.shape("circle")
+        self.penup()
