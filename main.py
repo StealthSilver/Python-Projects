e2f9ca4c56762bccb8d7a0be30aed60d87731c9b
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-20 07:49:15 +0530

    refactoring

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
-from turtle import Screen 
+from turtle import Screen, Turtle 
 
 screen = Screen()
 screen.title("PONG")
 screen.bgcolor("black")
 screen.setup(width=880 , height=600)
+
+paddle = Turtle()
+paddle.shape("Square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5 , stretch_len=1)
+paddle.penup()
+paddle.goto(350 , 0)
+
+def go_up():
+    new_y = paddle.ycor() + 28
+    paddle.goto(paddle.xcor() , new_y)
+
+def go_down():
+    new_y = paddle.ycor() - 28
+    paddle.goto(paddle.xcor() , new_y)
+
+
+
+screen.listen()
+screen.onkey(go_up , "Up")
 
 
 screen.exitonclick()
